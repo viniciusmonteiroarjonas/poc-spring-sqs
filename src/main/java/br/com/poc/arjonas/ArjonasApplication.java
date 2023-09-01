549f4bc0c9151fe696b17b971bf3c847a4bbefa1
Vinicius  Arjonas <viniciusmonteiroarjonas@outlook.com>
2023-09-01 19:23:00 -0300

    criado a pasta de projeto incial

---
+++ src/main/java/br/com/poc/arjonas/ArjonasApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc.arjonas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArjonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArjonasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/poc/arjonas/ArjonasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.poc.arjonas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArjonasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
